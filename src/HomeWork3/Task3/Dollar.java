f2dead72ec1ba82cfb838bdca00590b54939ce14
Alexandr <6023343@gmail.com>
2022-07-04 21:11:55 +0300

    Initial commit

---
+++ src/HomeWork3/Task3/Dollar.java
@@ -0,0 +1,11 @@
+package HomeWork3.Task3;
+
+public class Dollar extends Money{
+
+
+    public Dollar(Integer convertToRub) {
+        super("dollar", convertToRub);
+    }
+
+
+}
---
+++ src/HomeWork3/Task3/Euro.java
@@ -0,0 +1,8 @@
+package HomeWork3.Task3;
+
+public class Euro extends Money{
+
+    public Euro(Integer convertToRub) {
+        super("euro", convertToRub);
+    }
+}
